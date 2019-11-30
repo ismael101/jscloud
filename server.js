58c4e2389ee7f4e933c2e09b9cafae697293a3f3
= <=>
2019-11-30 00:16:23 -0600

    connected front and backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const users = require('./routes/user')
 const files = require('./routes/file')
 const images = require('./routes/image')
+const path = require('path')
 const app = express()
 
 mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds235378.mlab.com:35378/cloud_db`,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true})
@@ -25,6 +26,12 @@ app.use('/uploads/users', express.static('uploads/users'))
 app.use('/users', users)
 app.use('/files', files)
 app.use('/images', images)
+
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.use((req,res,next) => {
 	res.status(404).send("Not Found")
